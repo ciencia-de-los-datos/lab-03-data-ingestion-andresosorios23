67424b8750f8b5e8aab4198b4af5043cbba365fa
andresosorios23 <anosoriosa@unal.edu.co>
2024-04-06 10:02:21 -0500

    intento 1

--- pregunta.py
+++ pregunta.py
@@ -9,13 +9,30 @@ espacio entre palabra y palabra.
 
 
 """
+
 import pandas as pd
+import re
 
 
 def ingest_data():
-
-    #
-    # Inserte su código aquí
-    #
-
+    df = pd.read_fwf("clusters_report.txt", widths=[8, 16, 16, 80], header=[0, 1])
+    df.columns = [
+        re.sub(
+            r"\s+(?=\w)", "_", " ".join([x for x in col if "Unnamed" not in x]).lower()
+        ).strip()
+        for col in df.columns
+    ]
+    df = df.ffill()
+    duplicates_mask = df.duplicated(subset="cluster")
+    for index, row in df[duplicates_mask].iterrows():
+        original_index = df.index[df["cluster"] == row["cluster"]].tolist()[0]
+        df.at[original_index, "principales_palabras_clave"] += (
+            ", " + row["principales_palabras_clave"]
+        )
+    df = df.drop_duplicates(subset="cluster")
+    df["principales_palabras_clave"] = df["principales_palabras_clave"].apply(
+        lambda x: re.sub(
+            r"\s+", " ", ", ".join([word.strip() for word in x.split(",") if word])
+        )
+    )
     return df
